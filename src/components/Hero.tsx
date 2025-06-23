c86ac9f0f9cd11eddc819c289cd3cee2cdc35338
HaymosBobby <haymosx@gmail.com>
2025-06-23 10:56:46 +0100

    hero sroll animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars, words" });
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
@@ -36,6 +41,26 @@ const Hero = () => {
       })
       .to(".right-leaf", { y: 200 }, 0)
       .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    if (videoRef.current) {
+      // Wait for video metadata to load so duration is available
+      videoRef.current.onloadedmetadata = () => {
+        gsap.to(videoRef.current, {
+          currentTime: videoRef.current!.duration || 1,
+          ease: "none",
+          scrollTrigger: {
+            trigger: "video",
+            start: startValue,
+            end: endValue,
+            scrub: true,
+            pin: true,
+          },
+        });
+      };
+    }
   }, []);
 
   return (
@@ -76,6 +101,18 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0 ">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          playsInline
+          preload="auto"
+        >
+          <source src="/videos/output.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
     </>
   );
 };
