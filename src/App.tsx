9e384ab1d725c701a1de7ea3f0874712e6f32313
HaymosBobby <haymosx@gmail.com>
2025-06-23 08:57:46 +0100

    first hero animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import gsap from "gsap";
 import Navbar from "./components/Navbar";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Hero from "./components/Hero";
 // import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
+    <main>
       <Navbar />
-    </div>
+      <Hero />
+      <div className="h-dvh bg-black"></div>
+    </main>
   );
 };
 
