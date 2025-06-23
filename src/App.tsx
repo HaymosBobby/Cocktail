9522a6df672d3a5de68b37fec46faeaa041d9e43
HaymosBobby <haymosx@gmail.com>
2025-06-23 08:20:47 +0100

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import { ScrollTrigger, SplitText } from "gsap/all";
+// import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl text-green-500">Cocktail</h1>
+      <Navbar />
     </div>
   );
 };
